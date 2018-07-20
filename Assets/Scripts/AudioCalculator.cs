0d461fcb1fa7279deec04ad9e176f14fa93ca4e0
DeanGouskos <cgouskos@myjmc.edu.au>
2018-07-20 16:01:04 +1000

    Progression2
    Physics done & complete!

---
+++ Assets/Scripts/AudioCalculator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioCalculator : MonoBehaviour {
+    public float BPM;
+    float BPS;
+    public float offset;
+	
+	void Start ()
+    {
+        BPS = BPM / 60;
+	}
+	
+	void Update ()
+    {
+        transform.Translate(-BPS * Time.deltaTime * offset, 0, 0);
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,33 +5,66 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     private Rigidbody rb;
+    public float speed;
+    public float jumpForce;
+    public float gravity;
+    private Vector3 moveDirection = Vector3.zero;
+    AudioCalculator audioCalculator;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-
     void Update()
     {
-        Vector3 vel = rb.velocity;
-        vel.x = 5;
-        rb.velocity = vel;
+        //Vector3 vel = rb.velocity;
+        //vel.x = speed;
+        //rb.velocity = vel;
         if (Physics.Raycast(transform.position, Vector3.down, GetComponent<BoxCollider>().size.y / 2 + 0.4f))
         {
-
+            
             Quaternion rot = transform.rotation;
-            rot.z = Mathf.Round(rot.z / 90) * 90;
+            rot.z = Mathf.Round(rot.z / 180) * 180;
             transform.rotation = rot;
-            if (Input.GetKeyDown(KeyCode.Space))
+            
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow))
             {
                 rb.velocity = Vector3.zero;
-                rb.AddForce(Vector2.up * 55000);
+                rb.AddForce(Vector2.up * jumpForce);            
             }
-            else
+           
+            if (Input.GetKeyDown(KeyCode.DownArrow))
             {
-                transform.Rotate(Vector3.back * 5f);
+                //Debug.Log("Down Active");
             }
+  
+        }
+        else
+        {
+            transform.Rotate(Vector3.back * 5f); 
+        }
+
+        /*PlayerController controller = GetComponent<CharacterController>();
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(1, 0, 0);
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            if (Input.GetKeyDown(KeyCode.Space))
+                moveDirection.y = jumpSpeed;
+
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);*/
+
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Obstacle")
+        {
+            Destroy(gameObject);
         }
     }
 }
