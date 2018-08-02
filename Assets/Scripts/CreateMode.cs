e6f3deb2c84f830a37d2b711731a47f7500b78d2
RaiXzr <41179919+RaiXzr@users.noreply.github.com>
2018-08-03 06:50:13 +1000

    Core Mechanic (DONE)
    Got the cube working as intended, now trying to find an easier way to create the level.

---
+++ Assets/Scripts/CreateMode.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateMode : MonoBehaviour {
+
+    public KeyCode key;
+    public bool createMode;
+    public GameObject spawn;
+		
+	void Update ()
+    {
+		if (createMode)
+        {
+            if (Input.GetKeyDown(key))
+                Instantiate(spawn, transform.position, Quaternion.identity);
+        }
+	}
+}
