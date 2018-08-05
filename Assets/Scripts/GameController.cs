69b7fbfac864997b4fc4eaf57cf2b1c97a0d93f8
RaiXzr <41179919+RaiXzr@users.noreply.github.com>
2018-08-05 23:22:59 +1000

    Working mechanic
    Level is playable, no bugs, no errors, just technical inconsistencies yet to be completely resolved.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    private bool levelComplete;
+    private bool gameOver;
+    private bool restart;
+    private bool quit;
+
+	void Start ()
+    {
+
+
+	}
+	
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+            {
+               SceneManager.LoadScene("Main");
+                //Debug.Log("restart");
+            }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Application.Quit();
+               // Debug.Log("Quit");
+            }
+  
+
+        //wait for seconds on wake??
+    }
+}
