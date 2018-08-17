eea0c31c520d960f2c4cc8b47fae86db5531b495
DeanGouskos <cgouskos@myjmc.edu.au>
2018-08-17 16:19:12 +1000

    Updated UI Progress
    A lot progress done for UI, still working on it and left with another level. Then all done!

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -9,38 +9,77 @@ public class GameController : MonoBehaviour {
     private bool isPaused;
     private bool levelComplete;
     private bool gameOver;
+    private bool resume;
     private bool restart;
     private bool quit;
 
-	void Start ()
+    bool Paused = false; 
+
+    public void ResumeLevel()
+    {
+
+    }
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    void Start ()
     {
 #if UNITY_EDITOR
         SceneManager.sceneLoaded += SceneLighting;
 #endif
     }
-	
-	void Update ()
+
+    void Update()
     {
         //if(Input.GetKeyDown(KeyCode.Escape))
-            //isPaused = true
-            //Deltatime 0
+        //isPaused = true
+        //Deltatime 0
 
         if (Input.GetKeyDown(KeyCode.R))
-            {
-               SceneManager.LoadScene("Main");
-                //Debug.Log("restart");
-            }
+        {
+            SceneManager.LoadScene("Main");
+            //Debug.Log("restart");
+        }
 
         if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            //isPaused
+            if (Paused == true)
             {
-                Application.Quit();
-               // Debug.Log("Quit");
+                Time.timeScale = 1.0f;
+                //Canvas.gameObject.SetActive(false);
+                //Screen.showCursor = false;
+                //Screen.lockCursor = true;
+                //Camera.audio.Play();
+                Paused = false;
             }
-  
+            else
+            {
+                Time.timeScale = 0.0f;
+                //Canvas.gameObject.SetActive(true);
+                //Screen.showCursor = true;
+                //Screen.lockCursor = false;
+                //Camera.audio.Pause();
+                Paused = true;
 
-        //wait for seconds on wake??
+                //PauseMenu
+
+                // Debug.Log("Quit");
+            }
+
+
+            //wait for seconds on wake??
+        }
     }
 
+    
 #if UNITY_EDITOR
     void SceneLighting(Scene scene, LoadSceneMode mode)
     {
