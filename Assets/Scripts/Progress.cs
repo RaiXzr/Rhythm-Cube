bfb1ba92de650b7db05fe5dc27084fc748c559b4
RaiXzr <41179919+RaiXzr@users.noreply.github.com>
2018-08-31 00:35:56 +1000

    Done_new level
    New level done. Booster still not working properly but might have a solution to it. Made UI for progress to finish,

---
+++ Assets/Scripts/Progress.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressScript : MonoBehaviour
+{
+
+    float StartPosition = 0;
+    float EndPosition = 2000;
+
+    Update()
+    {
+        float x = transform.position.x;
+        float normalized = (x - StartPosition) / EndPosition;
+
+        Debug.Log("Percentage Completed: " + (normalized * 100).ToString());
+    }
+}
